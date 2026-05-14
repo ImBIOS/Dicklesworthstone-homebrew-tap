4c789699107d571b8ec5d5c22626b40e5370a081
Dicklesworthstone <jeff141421@gmail.com>
2026-05-14 11:57:25 -0400

    chore(br): update formula to v0.2.10

--- Formula/br.rb
+++ Formula/br.rb
@@ -7,28 +7,28 @@
 class Br < Formula
   desc "Agent-first issue tracker with SQLite + JSONL sync"
   homepage "https://github.com/Dicklesworthstone/beads_rust"
-  version "0.1.21"
+  version "0.2.10"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/Dicklesworthstone/beads_rust/releases/download/v#{version}/br-v#{version}-darwin_arm64.tar.gz"
-      sha256 "0e2b96b6d89fdf7d5a1b8ae2d3b0fa5c5d739ea598f49b46c93629a0cbb0bdc1"
+      url "https://github.com/Dicklesworthstone/beads_rust/releases/download/v#{version}/br-#{version}-darwin_arm64.tar.gz"
+      sha256 "00ff833d0cb1ef0f651c75a6de0a08bed970bc4e7ef1075230a9fcbadadda372"
     end
     on_intel do
-      url "https://github.com/Dicklesworthstone/beads_rust/releases/download/v#{version}/br-v#{version}-darwin_amd64.tar.gz"
-      sha256 "d49d426147d6d7269fa3562178021c13dc2c006fdc417be0b08ac260be0453e6"
+      url "https://github.com/Dicklesworthstone/beads_rust/releases/download/v#{version}/br-#{version}-darwin_amd64.tar.gz"
+      sha256 "c76bacf98956416620385e74aa5e2d100ffa3d66a50b0eae65f895c902733a11"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/Dicklesworthstone/beads_rust/releases/download/v#{version}/br-v#{version}-linux_amd64.tar.gz"
-      sha256 "10d1ac74ce8eab761fb72ff632fc019edad75dd4d49c867c4655f53684d18832"
+      url "https://github.com/Dicklesworthstone/beads_rust/releases/download/v#{version}/br-#{version}-linux_amd64.tar.gz"
+      sha256 "f6388af90de58d1a98af720db061237147d964eff8fa52fdc8a50441f851f730"
     end
     on_arm do
-      url "https://github.com/Dicklesworthstone/beads_rust/releases/download/v#{version}/br-v#{version}-linux_arm64.tar.gz"
-      sha256 "50ac4fdd829e63d2b36158fad038855b19e5f17394a5fd1a09f970842e23b761"
+      url "https://github.com/Dicklesworthstone/beads_rust/releases/download/v#{version}/br-#{version}-linux_arm64.tar.gz"
+      sha256 "11ed13cd0a5ed50042de5e3d946dba9844c97fcc862a820cfd9a120464bbd99d"
     end
   end
 
